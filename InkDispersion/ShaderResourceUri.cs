0d6a3b89614924774b9a9aa1b729572cc5957be1
routersys <github.routersys@gmail.com>
2026-09-22 12:16:33 +0900

    シェーダーの生成をInkDispersion.Shaderへ切り出す

--- InkDispersion/ShaderResourceUri.cs
+++ InkDispersion/ShaderResourceUri.cs
@@ -2,5 +2,5 @@ namespace InkDispersion;
 
 internal static class ShaderResourceUri
 {
-    public static Uri Get(string shaderName) => new($"pack://application:,,,/InkDispersion;component/Shaders/{shaderName}.cso", UriKind.Absolute);
+    public static Uri Get(string shaderName) => new($"pack://application:,,,/InkDispersion;component/Resources/Shader/{shaderName}.cso", UriKind.Absolute);
 }
